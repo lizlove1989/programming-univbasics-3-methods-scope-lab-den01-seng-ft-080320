8a3254755f1a6d7052430abfeea98efead8c6850
Liz Love <love.elizabeth1989@gmail.com>
2020-04-29 21:46:43 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -5,7 +5,5 @@ def mario
 end
 
 def toadstool
-  status = "Thank You Mario! But Our Princess Is In Another Castle!"
-  phrase "It's Dangerous To Go Alone! Take This."
-  puts phrase
+  puts status
 end
